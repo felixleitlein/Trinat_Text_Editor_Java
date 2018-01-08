10b46748c06aabd79a63bf8767e087aed4f59d38
felixleitlein <felix.leitlein@students.fhnw.ch>
2018-01-08 17:19:24 +0100

    version 1.1

--- src/trinat/bloch/editor/Texteditor_Bloch.java
+++ src/trinat/bloch/editor/Texteditor_Bloch.java
@@ -1,6 +1,15 @@
 package trinat.bloch.editor;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -8,12 +17,18 @@ import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
-// Version 0$4
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyledDocument;
+
+
+// Version 1$1
+// mit JMenuItem NEU
 public class Texteditor_Bloch {
 
 	private JFrame rahmen;
 	private JTextPane editor;
-
+	private File datei;
+	
 	public static void main(String[] args) throws Exception {
 
 		SwingUtilities.invokeLater(new Runnable() {
@@ -39,7 +54,7 @@ public class Texteditor_Bloch {
 		JMenu fileMenu = new JMenu("Datei");
 		
 		JMenuItem saveItem = new JMenuItem("Speichern");
-
+		saveItem.addActionListener(new SaveFileListener());
 		JMenuItem openItem = new JMenuItem("Oeffnen");
 
 		JMenuItem newItem= new JMenuItem("Neu");
@@ -62,5 +77,51 @@ public class Texteditor_Bloch {
 
 		editor.requestFocusInWindow();
 
+	}
+	private class SaveFileListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (datei == null) {
+
+				datei = chooseFile();
+
+				if (datei == null) {
+
+					return;
+				}
+			}
+
+			DefaultStyledDocument doc = (DefaultStyledDocument) getEditorDocument();
+
+			try (OutputStream fos = new FileOutputStream(datei);
+					ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+				oos.writeObject(doc);
+			} catch (IOException ex) {
+
+				throw new RuntimeException(ex);
+			}
+
+		}
+
+		private File chooseFile() {
+
+			JFileChooser chooser = new JFileChooser();
+
+			if (chooser.showSaveDialog(rahmen) == JFileChooser.APPROVE_OPTION) {
+
+				return chooser.getSelectedFile();
+			} else {
+				return null;
+			}
+		}
+	}
+	
+	private StyledDocument getEditorDocument() {
+
+		StyledDocument doc = (DefaultStyledDocument) editor.getDocument();
+		return doc;
 	}
 }
